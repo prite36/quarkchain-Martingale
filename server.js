cffe5da75ff179a00b8d693496c3ce8a5382eb05
prite36 <spritelemon36@hotmail.com>
2018-07-20 01:30:51 +0700

    no message

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const Nightmare = require("nightmare")
 const nightmare = Nightmare({ show: true })
+const wallet = require("./wallet.js")
+const betAmount ='0.05'
+
 nightmare
-  .goto('https://duckduckgo.com')
-  .type('#search_form_input_homepage', 'github nightmare')
-  .click('#search_button_homepage')
-  .wait('#r1-0 a.result__a')
-  .evaluate(() => document.querySelector('#r1-0 a.result__a').href)
-  .end()
-  .then(console.log)
-  .catch(error => {
-    console.error('Search failed:', error)
-  })
+    .goto('https://testnet.quarkchain.io/app/simple-game')
+    .insert('#pk_input', wallet)
+    .wait(1000)
+    .click('.btn-primary')
+    //   .evaluate(() => {
+    //     return document.querySelector(".simple-game-status").innerText
+    //   })
+    //   .then(text => {
+    //       console.log(text);  
+    //   })
+    .wait("#bet_amount") 
+    .evaluate(() =>{
+        document.querySelector('#bet_amount').value = ''
+    })
+    .insert('#bet_amount', betAmount)
+    //   .click('.btn.btn-primary.btn-sm.collapsed')
+    .catch(error => {
+        console.error('Search failed:', error)
+    })
